177f4f50c33a60f921721c058b839faf06e0f750
Kenechi <kenechicynthia5@gmail.com>
2021-10-04 13:48:12 +0100

    two files created

---
+++ src/App.js
@@ -0,0 +1,26 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className='App'>
+        <div className="header">
+     <h1>building a site in react, npso interesting</h1>
+    </div>
+    <div className='body1'>
+      <ul>
+        <h2>the following are obtainable in my react App</h2>
+        <li>introduction to html</li>
+        <li>introduction to css</li>
+        <li>introduction to javascript</li>
+        </ul>
+    </div>
+    </div>
+    
+);
+}
+
+
+
+
+export default App;
